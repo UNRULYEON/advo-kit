d32a464f17932bb2e3df8e077af2f65d948758b0
Amar Kisoensingh <amar@kisoensingh.sh>
2022-10-26 19:46:04 +0200

    Adds tooltip to button

--- src/components/CoolblueLogoButton.tsx
+++ src/components/CoolblueLogoButton.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { motion, useAnimationControls, Variants } from 'framer-motion'
 import { FC } from 'react'
 import styled from 'styled-components'
 import CoolblueLogoText from './CoolblueLogoText'
@@ -8,25 +8,63 @@ type CoolblueLogoButtonProps = {
 }
 
 const CoolblueLogoButton: FC<CoolblueLogoButtonProps> = ({ onClick }) => {
+  const tooltipControls = useAnimationControls()
+  const tooltipVariants: Variants = {
+    hide: {
+      opacity: 0,
+      translateY: 80,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    show: {
+      opacity: 1,
+      translateY: 80,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  }
+
   return (
-    <ButtonStyled>
-      <ButtonSideStyled />
-      <ButtonFaceStyled
-        onClick={() => onClick()}
-        initial={{ translateY: 6 }}
-        whileHover={{ translateY: 8 }}
-        whileTap={{ translateY: 0.1 }}
-      >
-        <CoolblueLogoText width={60} height={60} />
-      </ButtonFaceStyled>
-    </ButtonStyled>
+    <ButtonContainer>
+      <ButtonStyled>
+        <ButtonSideStyled />
+        <ButtonFaceStyled
+          onClick={() => onClick()}
+          initial={{ translateY: 6 }}
+          whileHover={{ translateY: 8 }}
+          whileTap={{ translateY: 0.1 }}
+          onHoverStart={() => tooltipControls.start('show')}
+          onHoverEnd={() => tooltipControls.start('hide')}
+        >
+          <CoolblueLogoText width={60} height={60} />
+          <TooltipStyled
+            initial={'hide'}
+            animate={tooltipControls}
+            variants={tooltipVariants}
+          >
+            Click me to start
+          </TooltipStyled>
+        </ButtonFaceStyled>
+      </ButtonStyled>
+    </ButtonContainer>
   )
 }
 
-const ButtonStyled = styled.div`
-  position: relative;
+const ButtonContainer = styled.div`
+  position: absolute;
+  width: 400px;
+  height: 300px;
+  transform: translateZ(401px);
 
-  transform: translateZ(401px) translateY(195px) translateX(150px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const ButtonStyled = styled(motion.div)`
+  position: relative;
 `
 const ButtonSideStyled = styled.div`
   position: absolute;
@@ -52,6 +90,21 @@ const ButtonFaceStyled = styled(motion.div)`
   :hover {
     cursor: pointer;
   }
+`
+
+const TooltipStyled = styled(motion.div)`
+  position: absolute;
+  padding: 8px 12px;
+
+  border-radius: 4px;
+  white-space: nowrap;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+
+  background-color: #111111;
+
+  user-select: none;
 `
 
 export default CoolblueLogoButton
