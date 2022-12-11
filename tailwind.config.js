7770ec855f58f8ffb9374862a556e37dec154d01
Amar Kisoensingh <amar@kisoensingh.sh>
2022-12-11 14:10:33 +0100

    Adds storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
+    fontFamily: {
+      sans: ['Open Sans', ...fontFamily.sans],
+    },
     extend: {
+      spacing: {
+        'fill-available': '-webkit-fill-available'
+      },
       colors: {
         'coolblue': '#0090E3',
         'dark-blue': '#285DAB',
         'hot-orange': '#FF6B00',
+        'cool-black': '#111111',
+        'gray-blue': '#F2F7FC',
+        'silver': '#BBBBBB',
       },
     },
   },
