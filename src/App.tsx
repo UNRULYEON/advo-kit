7208d86ab4da3bcd0bba7c652e3e66f845fc717a
Amar Kisoensingh <amar@kisoensingh.sh>
2022-12-16 00:19:32 +0100

    Add components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import FactSlogan from "@components/FactSlogan";
 import KitContext from "@components/KitContext";
+import KitDropdown from "@components/KitDropdown";
 import Menu from "@components/Menu";
 
 const App = () => {
   return (
     <KitContext>
-      <div></div>
+      <main className="flex flex-col w-full h-full bg-gray-blue">
+        <div className="flex w-full p-3 lg:p-10 flex-col gap-2 sm:gap-0 sm:flex-row">
+          <div className="sm:flex-1">
+            <FactSlogan fact="Advo-kit" slogan="Think outside the box" />
+          </div>
+          <div className="sm:flex-1 md:max-w-[240px]">
+            <KitDropdown />
+          </div>
+        </div>
+        <div className="flex-1">box</div>
+        <div className="flex w-full p-3 lg:p-10 flex-col gap-2 sm:gap-0 sm:flex-row">
+          <div className="flex-1">
+            <Menu />
+          </div>
+        </div>
+      </main>
     </KitContext>
   );
 };
