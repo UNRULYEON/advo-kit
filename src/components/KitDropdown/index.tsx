eef0c88e525060cae8f0a823fb11270b4fbe2a4f
Amar Kisoensingh <amar@kisoensingh.sh>
2022-12-16 00:14:27 +0100

    Created kit dropdown component

---
+++ src/components/KitDropdown/index.tsx
@@ -0,0 +1,31 @@
+import Dropdown, { Item } from "@components/Dropdown";
+import { useKitContext } from "@components/KitContext";
+
+const KitDropdown = () => {
+  const { kits, currentKit, setCurrentKit } = useKitContext();
+
+  const kitItems: Item[] = kits.map((kit) => ({
+    id: kit.id,
+    name: kit.name,
+  }));
+
+  const currentItem: Item = {
+    id: currentKit.id,
+    name: currentKit.name,
+  };
+
+  const handleOnClick = (item: Item) => {
+    const kit = kits.find((kit) => kit.id === item.id);
+    if (kit) setCurrentKit(kit);
+  };
+
+  return (
+    <Dropdown
+      handleOnClick={handleOnClick}
+      currentItem={currentItem}
+      items={kitItems}
+    />
+  );
+};
+
+export default KitDropdown;
