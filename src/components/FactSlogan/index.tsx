f32c12e29548f631271861794345772c7f4324e4
Amar Kisoensingh <amar@kisoensingh.sh>
2022-12-16 00:19:23 +0100

    Add small fact/slogan size

--- src/components/FactSlogan/index.tsx
+++ src/components/FactSlogan/index.tsx
@@ -7,11 +7,11 @@ type FactSloganProps = {
 
 const FactSlogan: FC<FactSloganProps> = ({ fact, slogan }) => {
   return (
-    <div className="flex flex-col">
-      <span className="text-dark-blue font-[AvenirBlack] text-[58.1188px] tracking-[-0.025em] mb-[-30px] uppercase after:content-['.'] after:text-hot-orange">
+    <div className="flex flex-col columns-1">
+      <span className="text-dark-blue font-[AvenirBlack] text-[40px] md:text-[45px] lg:text-[58.1188px] tracking-[-0.025em] mb-[-20px] lg:mb-[-30px] uppercase after:content-['.'] after:text-hot-orange">
         {fact}
       </span>
-      <span className="text-coolblue font-[DobraBook] text-[45.2035px] tracking-[-0.025em] after:content-['.']">
+      <span className="text-coolblue font-[DobraBook] text-[31px] md:text-[35px] lg:text-[45.2035px] tracking-[-0.025em] after:content-['.']">
         {slogan}
       </span>
     </div>
@@ -19,3 +19,12 @@ const FactSlogan: FC<FactSloganProps> = ({ fact, slogan }) => {
 };
 
 export default FactSlogan;
+
+// 58 | 100%
+// 45 | 77.5%
+
+// 45 | 100%
+// 35 | 77.5%
+
+// 40 | 100%
+// 31 | 77.5%
