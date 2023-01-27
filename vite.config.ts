b99f9cd19fd10c61624868ffd53d62b6f2913fc9
Amar Kisoensingh <amar@kisoensingh.sh>
2023-01-27 22:06:37 +0100

    Create useOnClickOutside hook

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     alias({
       entries: {
         '@components': path.resolve(__dirname, './src/components'),
+        '@hooks': path.resolve(__dirname, './src/hooks'),
         '@icons': path.resolve(__dirname, './src/icons'),
         '@utils': path.resolve(__dirname, './src/utils'),
         '@kits': path.resolve(__dirname, './src/kits.ts'),
