480abfa58c8dcb0d94cca332be304e43987dd578
Amar Kisoensingh <amar@kisoensingh.sh>
2023-02-06 22:31:52 +0100

    Add card background (#19)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,6 +19,7 @@ module.exports = {
         'cool-black': '#111111',
         'gray-blue': '#F2F7FC',
         'evening-blue': '#1E4680',
+        navy: '#003366',
         silver: '#BBBBBB',
         gray: '#DDDDDD',
         brightgrey: '#eee',
