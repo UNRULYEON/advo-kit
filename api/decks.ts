5829d8e1526e15e3d54e1b5e2f630098aa2a21bf
Amar Kisoensingh <amar@kisoensingh.sh>
2023-03-25 19:41:17 +0100

    Add PWA functionality

--- api/decks.ts
+++ api/decks.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { readFileSync } from 'fs';
-import path from 'path';
+const path = require('path');
+// import path from 'path';
 
 export default async function handler(_: NextApiRequest, response: NextApiResponse) {
   const file = path.join(process.cwd(), 'decks/transformed/decks.json');
