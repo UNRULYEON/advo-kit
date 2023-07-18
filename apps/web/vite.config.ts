55c0a62a9d9bbc013810f361fa3ffa02e7df3fc2
Amar Kisoensingh <amar@kisoensingh.sh>
2023-07-18 22:57:33 +0200

    Remove generation files

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    https: {
+      key: "../../localhost-key.pem",
+      cert: "../../localhost.pem",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
